7d1d7e2e43fbf37ba7294a69968cdabee6ac3086
Diego Michel Rubio Ramirez <diegomichel@gmail.com>
2017-01-16 10:13:48 -0700

    #15 Adds tracking code and configuration for each environment staging/prod

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the Rails application.
 require_relative 'application'
 
+AppConfig = YAML.load(File.read(Rails.root + 'config' + 'config.yml'))[Rails.env].with_indifferent_access
+
 # Initialize the Rails application.
 Rails.application.initialize!
