29bcc82e32b0a8225fccbf8fa84f11f0978cf11e
Diego Michel Rubio Ramirez <diegomichel@gmail.com>
2017-01-18 12:13:46 -0700

    #15 Adds special case for closing popup and removes console.log

--- app/assets/javascripts/google_analytics.js
+++ app/assets/javascripts/google_analytics.js
@@ -2,7 +2,9 @@ $(function(){
   "use strict";
   $('a, button').on("click", function(event){
     var text = event.currentTarget.textContent.trim();
+    if(text === '×'){
+      text = 'close popup';
+    }
     ga('send', 'event', 'link', 'click', text);
-    console.log("Event label: "+text);
   });
 });
